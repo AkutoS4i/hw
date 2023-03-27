f7229e507fcf124292b56f105f3e616c53befcc8
Daniil <124083006+AkutoS4i@users.noreply.github.com>
2023-03-28 01:21:27 +0300

    .

---
+++ WebLogin/WebLogin/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebLogin.Models;
+
+namespace WebLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult LoginError()
+        {
+            return NotFound("Возникли проблемы со входом");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public RedirectResult Login(string username, string password)
+        {
+            string startUsername = "admin";
+            string startPassword = "admin";
+            if (startUsername.Equals(username) && startPassword.Equals(password))
+            {
+                return Redirect("~/Home/Index");
+            }
+            else return Redirect("~/HomeController/LoginError");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
